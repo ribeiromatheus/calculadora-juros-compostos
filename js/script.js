60caba1622221be77723f5581d9780fc80eecd91
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-04-01 15:26:18 -0300

    Update 01/04/19

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 $(document).on('change', '.dataput', function () {
-    var prazoAno = $('.prazoAno').val();
-    var vencem = $('.ateVencer').val();
+    let prazoAno = $('.prazoAno').val(),
+        vencem = $('.ateVencer').val();
     if (vencem) {
-        var compDateParse = Date.parse(vencem);
+        let compDateParse = Date.parse(vencem);
         console.log(compDateParse);
 
-        var currentDate = new Date();
-        var currentParse = Date.parse(currentDate);
+        let currentDate = new Date();
+        let currentParse = Date.parse(currentDate);
         console.log(currentParse);
 
-        var daysDiff = (compDateParse - currentParse) / (1000 * 60 * 60 * 24 * 30);
+        let daysDiff = (compDateParse - currentParse) / (1000 * 60 * 60 * 24 * 30);
         $('.prazo').val(daysDiff.toFixed(2));
     } else if (prazoAno)
         $('.prazo').val((parseFloat(prazoAno) * 12).toFixed(2));
 })
     .on('change', '.taxaAno', function () {
-        var taxaobj = $('.taxa');
-        var taxaAno = $('.taxaAno').val() * 1;
+        let taxaobj = $('.taxa');
+        let taxaAno = $('.taxaAno').val() * 1;
         taxaobj.val(((Math.pow((taxaAno / 100 + 1), (1 / 12)) - 1) * 100).toFixed(2));
     })
     .on('click', '.simulaUm', function () {
         /* formatadores */
         Number.prototype.praPessoa = function (c, d, t) {
-            var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c,
+            let n = this, c = isNaN(c = Math.abs(c)) ? 2 : c,
                 d = d == undefined ? ',' : d,
                 t = t == undefined ? '.' : t,
                 s = n < 0 ? '-' : '',
@@ -35,13 +35,13 @@ $(document).on('change', '.dataput', function () {
         jQuery('.resultados').show();
         jQuery('html, body');
 
-        var montante = parseFloat(jQuery('.montante').val() * 1),
+        let montante = parseFloat(jQuery('.montante').val() * 1),
             aporte = parseFloat(jQuery('.aporte').val() * 1),
             prazo = parseFloat(jQuery('.prazo').val() * 1),
             taxa = parseFloat(jQuery('.taxa').val()) / 100;
 
         /* Arrays para gráfico */
-        var dados = [],
+        let dados = [],
             dadosjuros = [],
             dadosAportes = [];
         dados.push([0, montante]);
@@ -49,7 +49,7 @@ $(document).on('change', '.dataput', function () {
         dadosAportes.push([0, montante]);
 
         /* Cálculo Mensal */
-        var jurosout = 0,
+        let jurosout = 0,
             acumulado,
             acuPush = 0,
             aportePush = 0,
@@ -69,15 +69,15 @@ $(document).on('change', '.dataput', function () {
             acuPush = Math.floor((acuPush + parseInt((tempJuros + aporte) * 100) / 100) * 100) / 100;
             console.log('anterior: ' + acuPush + ' juros: ' + tempJuros + ' aporte ' + aporte);
             dados.push([i, acuPush]);
-            
+
             /* calcula acumulado de aportes */
             aportePush = montante + aporte * i;
             dadosAportes.push([i, aportePush]);
-            
+
             /* calcula os juros acumulados */
             jurosPush = Math.floor(parseFloat(acuPush - aportePush).toFixed(2) * 100) / 100;
             dadosjuros.push([i, jurosPush]);
-            
+
             /* incrementa tabela */
             acumulado += '<tr><td>' + i + '</td><td>' + Number(aportePush).praPessoa() + '</td><td>' + Number(tempJuros).praPessoa() + '</td><td>' + Number(jurosPush).praPessoa() + '</td><td>' + Number(acuPush).praPessoa() + '</td></tr>';
         }
@@ -98,15 +98,15 @@ $(document).on('change', '.dataput', function () {
         console.log(acumulado);
         jQuery('#printfinal').find('tbody').html(acumulado);
         /* gera grafico */
-        // var line_cresce = { label: 'Acumulado', data: dados, color: '#3c8dbc' };
-        // var aporte_cresce = { label: 'Investimento', data: dadosAportes, color: '#ff3333' };
-        // var juros_cresce = { label: 'Juros', data: dadosjuros, color: '#47d147' };
+        // let line_cresce = { label: 'Acumulado', data: dados, color: '#3c8dbc' };
+        // let aporte_cresce = { label: 'Investimento', data: dadosAportes, color: '#ff3333' };
+        // let juros_cresce = { label: 'Juros', data: dadosjuros, color: '#47d147' };
         /* ajusta legenda*/
         // jQuery('<div id='ctooltip'></div>').css({ position: 'absolute', display: 'none', border: '1px solid #fdd', padding: '2px', 'background-color': '#fee', opacity: 0.80 }).appendTo('body');
         // jQuery('#cresce-chart').bind('plothover', function (event, pos, item) {
-        //     var str = '(' + pos.x.toFixed(2) + ', ' + pos.y.toFixed(2) + ')';
+        //     let str = '(' + pos.x.toFixed(2) + ', ' + pos.y.toFixed(2) + ')';
         //     $('#hoverdata').text(str); if (item) {
-        //         var x = item.datapoint[0].toFixed(0), y = item.datapoint[1].toFixed(2); jQuery('#ctooltip').html(item.series.label + ' de ' + y + ' no mês ' + x).css({ top: item.pageY + 5, left: item.pageX + 5 }).fadeIn(200);
+        //         let x = item.datapoint[0].toFixed(0), y = item.datapoint[1].toFixed(2); jQuery('#ctooltip').html(item.series.label + ' de ' + y + ' no mês ' + x).css({ top: item.pageY + 5, left: item.pageX + 5 }).fadeIn(200);
         //     } else {
         //         $('#ctooltip').hide();
         //     }
